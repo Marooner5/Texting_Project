5f5ab814b0426606ac6c137f23b78c39e7837e92
Marooner5 <Drewlit@me.com>
2013-09-28 23:27:47 -0500

    Update
    Cycle through row if current key == previous otherwise return newletter.

--- src/Multitap.java
+++ src/Multitap.java
@@ -3,21 +3,29 @@
  */
 
 /**
- * @author Andrew and Fin
- *
+ * @author dela5875
+ * 
  */
-public class Multitap extends TestKeypad {
-	
-	private boolean upperCase = false;
-	
+public class Multitap {
+	private int presses = 0;
+	private int previousKey = -1;
+	private String[][] keys = new String[][] {
+			{ "next", "next", "a", "d", "g", "j", "m", "p", "t", "w", "shift", "space" },
+			{ "next", "next", "b", "e", "h", "k", "n", "q", "u", "x", "shift", "space" },
+			{ "next", "next", "c", "f", "i", "l", "o", "r", "v", "y", "shift", "space" },
+			{ "next", "next", null, null, null, null, null, "s", null, "z", "shift", "space" } 
+		};
+	private int currentLetter = 0;
 
-	/**
-	 * @param method
-	 * @p
+	public String append(String txt, int key) {
+		String newLetter = "";
+		String result;
+		if (presses == 0) {
+			previousKey = key;
+		}
+		if (previousKey == key) {
+			presses++;
+		}
+		return newLetter;
 	}
-	
-	public void reset(){
-		
-	}
-	
-	
+}
