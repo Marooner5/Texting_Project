1bcac688802584aebcee7e76a69a6d487431733d
Andrew Littler <drewlit@me.com>
2013-09-28 23:05:05 -0500

    Creation

---
+++ src/Multitap.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Andrew and Fin
+ *
+ */
+public class Multitap extends TestKeypad {
+	
+	private boolean upperCase = false;
+	
+
+	/**
+	 * @param method
+	 * @p
+	}
+	
+	public void reset(){
+		
+	}
+	
+	
